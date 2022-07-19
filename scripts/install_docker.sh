d7eef6457741ef1aeede55a143e73abfc0f91fbf
Ching-lin LEE <chilee@student.42.fr>
2022-07-19 14:39:15 +0200

    1907

---
+++ scripts/install_docker.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#install docker
+echo "installing docker..."
+#download docker (https://docs.docker.com/engine/install/ubuntu/#install-using-the-convenience-script)
+sudo curl -fsSL https://get.docker.com -o get-docker.sh
+#install docker
+sudo sh ./get-docker.sh
+
