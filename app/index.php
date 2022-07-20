5cb0fb932fb86445703910ee1e52db1a86616d91
Ching-lin LEE <chilee@student.42.fr>
2022-07-20 17:29:15 +0200

    update2007v4

---
+++ app/index.php
@@ -0,0 +1,13 @@
+<html>
+ <head>
+  <title>Test PHP</title>
+ </head>
+ <body>
+</br></br></br>
+<center>
+ <?php 
+    echo "Bonjour tout le monde<p>"; 
+    echo '<img src="./images/Pikachu.png" alt="pikachu">';?>
+</center>
+</body>
+</html>
