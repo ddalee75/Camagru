ab8fe64ad2fc1cde82bdf2f3823fcf4dd1e8e4c7
ddalee75 <ddalee75@gmail.com>
2022-07-29 15:15:28 +0200

    update2907

---
+++ app/classes/login_ctl.classes.php
@@ -0,0 +1,39 @@
+<?php
+
+class LoginContr extends Login
+{
+    private $uid; 
+    private $pwd; 
+
+    public function __construct($uid, $pwd)
+    {
+        $this->uid = $uid;
+        $this->pwd = $pwd;
+    }
+
+    // checker si les champs sont vide
+    private function emptyInput()
+    {
+        $result;
+        if(empty($this->uid) || empty($this->pwd))
+        {
+            $result = false;
+        }    
+        else {
+            $result = true;
+        }
+        return $result;
+    }
+
+    public function loginUser()
+    {
+        if($this->emptyInput() == false)
+        {
+            // echo "Empty input!";
+            header("location: ../index.php?error=emptyinput");
+            exit();
+        }
+
+        $this->getUser($this->uid, $this->pwd);
+    }
+}
---
+++ app/includes/login.inc.php
@@ -0,0 +1,22 @@
+
+<?php
+//Grabbing the data
+if(isset($_POST["submit"]))
+{
+    
+    $uid = $_POST["uid"];
+    $pwd = $_POST["pwd"];
+
+// Instantiate SignupContr class
+    include "../classes/dbh.classes.php";
+    include "../classes/login.classes.php";
+    include "../classes/login_ctl.classes.php";
+    $login = new LoginContr($uid, $pwd);
+
+// Running error handlers and user signup
+
+    $login->loginUser();
+    
+// Going to back to front page
+    header("location: ../index.php?error=none");
+}
