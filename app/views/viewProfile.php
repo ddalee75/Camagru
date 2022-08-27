da54615dafa60ee7e23ac679f983900b3a0b66b5
ddalee75 <ddalee75@gmail.com>
2022-08-27 20:04:42 +0200

    update270822v2

--- app/views/viewProfile.php
+++ app/views/viewProfile.php
@@ -8,15 +8,51 @@
     <title>Chilee's Camagru</title>
 </head>
 <body>
+<?php
 
+
+
+
+
+?>
 <div class="all_profile">
     <div class="titre_profile">
-    <h3>My Profile</h3></div>
+    <h3 type="text">My Profile</h3></div>
+
     <div class="env_form">
-        <div class="photo_form"></div>
-        <div class="content_form"></div>
-    </div>
-</div>
+        <div class="photo_form">
+        </div><!-- fin  photo_form -->
+
+
+        <div class="content_form">
+            <div class="titre_form_profile">Modify your informations</div>
+
+            <form action="http://localhost/controllers/ControllerProfile.php" method="post">
+            <label for="uid">Your login*</label>
+            <input type="text" id="uid" name="uid" value="John" required="required">
+
+            <!-- <label for="password">Your password*</label>
+            <input type="password" id="password" name="password" value="******" required="required"> -->
+
+            <label for="email">Your email*</label>
+            <input type="text" id="email" name="email" value="doe@gmail.com" required="required">
+
+            <label for="notification">Be notified by email?</label>
+            <input type="checkbox" id="notification" name="notification" value="yes" checked> <br>
+            <div class="text_r">* Required Field</div>
+           
+                <div class="btn_profile">
+                <div class="txt_pwd"><a href="../index.php?url=Resetpassword">Change Password, Clic here !</a></div>
+                <button type="submit_profile" name="submit">Modify</button>
+                </div>
+       
+
+            </form>
+
+
+        </div><!-- fin  content_form -->
+    </div><!-- fin  env_form -->
+</div><!-- fin all_profile -->
 
 
 </body>
